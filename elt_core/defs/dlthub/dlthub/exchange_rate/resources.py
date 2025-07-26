b50c2eff95a63e8ef62f2aed2b170f14b368f163
Andrew Staus <adstaus@hotmail.com>
2025-07-25 23:18:06 -0600

    cleaned up styling

--- elt_core/defs/dlthub/dlthub/exchange_rate/resources.py
+++ elt_core/defs/dlthub/dlthub/exchange_rate/resources.py
@@ -1,11 +1,14 @@
-from typing import Callable, Any
+from typing import Callable, Any, Generator
 import requests
 
 def get_exchange_rate(currency: str) -> Callable[[], Any]:
 
-    uri = f"https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@2025-01-01/v1/currencies/{currency}.json"
+    uri = ("https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api"
+           "@latest"
+            "/v1/"
+            f"currencies/{currency}.json")
 
-    def exchange_api():# -> Any:
+    def exchange_api() -> Generator[Any, Any, None]:# -> Any:
         response = requests.get(uri)
         yield response.json()
     
