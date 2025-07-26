274730f627597c25cab00758409997eaaf36130a
Andrew Staus <adstaus@hotmail.com>
2025-07-25 21:37:10 -0600

    added exchange rate data

--- dbt/models/staging/exchange_rate/stg_exchange_rate__cad.sql
+++ dbt/models/staging/exchange_rate/stg_exchange_rate__cad.sql
@@ -10,12 +10,12 @@ with cad as (
 
 renamed as (
     select
-    {% for col in adapter.get_columns_in_relation(source("exchange_rate", "cad")) %}
-        {{ col.name }} "{{ col.name.split("__", maxsplit=1)[-1].upper() -}}"
-        {%- if not loop.last -%}
-        ,
-        {%- endif -%}
-    {% endfor %}
+        {% for col in adapter.get_columns_in_relation(source("exchange_rate", "cad")) -%}
+            {% if not col.name.lower().startswith("_dlt") %}
+            {{- col.name }} "{{ col.name.split("__", maxsplit=1)[-1].upper() }}",
+            {% endif -%}
+        {% endfor %}
+        {{- dlt_loaded_at() }} _loaded_at
     from cad
 )
 
