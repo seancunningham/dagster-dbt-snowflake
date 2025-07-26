f66d0a0492755b186653e45d4043527805b0bfc8
Andrew Staus <adstaus@hotmail.com>
2025-07-25 23:10:07 -0600

    added more snippets

--- dbt/models/staging/adobe_experience/stg_adobe_experience__hits.sql
+++ dbt/models/staging/adobe_experience/stg_adobe_experience__hits.sql
@@ -1,25 +1,22 @@
 {{-
-  config(
-    materialized = "incremental",
-    incremental_strategy = "delete+insert",
-    unique_key = "hit_at",
-    meta = {
-      "dagster": {       
-        "partition": "daily",
-        "partition_start_date": "2025-07-01",
-        "automation_condition": "eager",
-        "freshness_check": {
-          "deadline_cron": "@daily",
-          "lower_bound_delta_seconds": 129600
+    config(
+        materialized = "incremental",
+        incremental_strategy = "delete+insert",
+        unique_key = "hit_at",
+        meta = {
+            "dagster": {
+                "partition": "daily",
+                "partition_start_date": "2025-07-01",
+                "automation_condition": "eager",
+                "freshness_check": {"deadline_cron": "@daily"}
+            }
         }
-      }
-    }
-  )
+    )
 -}}
 
 with
 web_hits as (
-    SELECT * FROM {{ source("adobe_experience", "web_hits") }}
+      SELECT * FROM {{ source("adobe_experience", "web_hits") }}
 ),
 
 app_hits as (
@@ -31,27 +28,27 @@ accounts as (
 ),
 
 united_hits as (
-  select 'web' hit_source, * from web_hits
-  union all
-  select 'app' hit_source, * from app_hits
+    select 'web' hit_source, * from web_hits
+    union all
+    select 'app' hit_source, * from app_hits
 )
 
 select
-  h.hit_source                                        hit_source,
-  {{ pst_to_utc("date_time") }}                       hit_at,
-  h.mcvisid                                           marketing_cloud_visitor_id,
-  concat_ws(':', h.post_visid_high, h.post_visid_low) visit_id,
-  concat_ws(':', visit_id, h.hitid_high, h.hitid_low) hit_id,
-  concat_ws(':', h.hit_source, hit_id)                hit_sid,
-  h.page_url                                          hit_url,
-  a.party_key                                         individual_party_key,
-  h.orderid                                           order_id,
-  h._sling_loaded_at                                  _loaded_at
+    h.hit_source                                        hit_source,
+    {{ pst_to_utc("date_time") }}                       hit_at,
+    h.mcvisid                                           marketing_cloud_visitor_id,
+    concat_ws(':', h.post_visid_high, h.post_visid_low) visit_id,
+    concat_ws(':', visit_id, h.hitid_high, h.hitid_low) hit_id,
+    concat_ws(':', h.hit_source, hit_id)                hit_sid,
+    h.page_url                                          hit_url,
+    a.party_key                                         individual_party_key,
+    h.orderid                                           order_id,
+    h._sling_loaded_at                                  _loaded_at
 from united_hits h
 left join accounts a on a.id = h.post_evar133
 
 {% if is_incremental() -%}
-  where 1=1 
-    and hit_at >= '{{ var("min_date", "1900-01-01") }}'
-    and hit_at <= '{{ var("max_date", "9999-12-31") }}'
+    where 1=1 
+      and hit_at >= '{{ var("min_date", "1900-01-01") }}'
+      and hit_at <= '{{ var("max_date", "9999-12-31") }}'
 {%- endif %}
